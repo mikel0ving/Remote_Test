ebb1d24647362831400726f1cdcad6fc2f871f8b
mikeloving <miguel.loyola1501@alumnos.ubiobio.cl>
2024-04-03 20:38:10 +0300

    Adding the program

---
+++ GitTest/GitTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOAD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------------------");
+            Console.WriteLine("enter the value of X");
+            string x = Console.ReadLine();
+            int X = Convert.ToInt32(x);
+            Console.WriteLine("enter the value of Y");
+            string y = Console.ReadLine();
+            int Y = Convert.ToInt32(y);
+            Console.WriteLine("Max value equal to: " + Math.Max(X, Y));
+        }
+    }
+}
